ebdd93f1c65c362563420a9fc866eb727098d56f
Falak Zahra <falak.zahra16@gmail.com>
2024-04-28 12:47:30 -0600

    Add assets and adjust seed files

--- seeds/halls.js
+++ seeds/halls.js
@@ -3,27 +3,27 @@ exports.seed = async function (knex) {
   await knex("halls").insert([
     {
       name: "Emerald Banquet Hall",
-      capacity: 150,
+      capacity: 300,
       description:
         "Our largest hall, adorned with crystal chandeliers and a classic decor. Perfect for weddings and large corporate events.",
       price: 5000,
-      hallImage_url: "http://localhost:8080/hall1",
+      hallImage_url: "http://localhost:8080/hall1.jpg",
     },
     {
       name: "Sapphire Conference Room",
-      capacity: 40,
+      capacity: 150,
       description:
         "A modern, tech-equipped space ideal for conferences, presentations, and workshops.",
-      price: 1500,
-      hallImage_url: "http://localhost:8080/hall2",
+      price: 2000,
+      hallImage_url: "http://localhost:8080/hall2.jpg",
     },
     {
-      name: "Ruby Boardroom",
-      capacity: 20,
+      name: "Ruby Lounge",
+      capacity: 75,
       description:
         "An intimate setting with luxurious furnishings for high-profile meetings and small gatherings.",
-      price: 750,
-      hallImage_url: "http://localhost:8080/hall3",
+      price: 950,
+      hallImage_url: "http://localhost:8080/hall3.jpg",
     },
   ]);
 };
