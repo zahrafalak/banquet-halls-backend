47e84acc9a9089eb268bffbb38c6c31ae97aa6a2
Falak Zahra <falakzahra@Falaks-MacBook-Pro.local>
2026-04-14 19:16:26 -0600

    feat: add user_id to booking_requests

--- migrations/20260415011306_add_user_id_to_bookings.js
+++ migrations/20260415011306_add_user_id_to_bookings.js
@@ -1,13 +1,11 @@
 exports.up = async (knex) => {
   await knex.schema.alterTable('booking_requests', (table) => {
     table.string('user_id').nullable();
-    table.string('status').defaultTo('pending');
   });
 };
 
 exports.down = async (knex) => {
   await knex.schema.alterTable('booking_requests', (table) => {
     table.dropColumn('user_id');
-    table.dropColumn('status');
   });
 };
